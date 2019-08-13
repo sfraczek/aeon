264c6dc5d066dfddaaeecaa0aab9492c57016e62
Sylwester Fraczek <sylwester.fraczek@intel.com>
2019-08-13 11:18:06 +0200

    dummy block manager might be complex. I have a new idea so I'm saving snapshot.

--- test/test_benchmark.cpp
+++ test/test_benchmark.cpp
@@ -15,10 +15,12 @@
 * limitations under the License.
 *******************************************************************************/
 
+#include <memory>
 #include "gtest/gtest.h"
 #include "file_util.hpp"
 #include "block_loader_file.hpp"
 #include "block_manager.hpp"
+#include "provider_factory.hpp"
 
 using namespace std;
 using namespace nervana;
@@ -50,11 +52,6 @@ TEST(benchmark, cache)
             iss >> manifest_filename;
         }
         string manifest = file_util::path_join(manifest_root, manifest_filename);
-
-        // chrono::high_resolution_clock                     timer;
-        // chrono::time_point<chrono::high_resolution_clock> start_time;
-        // chrono::time_point<chrono::high_resolution_clock> zero_time;
-        // chrono::milliseconds                              total_time{0};
 
         bool     shuffle_manifest = false;
         bool     shuffle_enable   = false;
@@ -111,6 +108,177 @@ TEST(benchmark, cache)
              << ", total time=" << setprecision(6) << total_time << ", average images/second "
              << total_count / total_time << endl;
     }
+}
+
+/* dummy_block_manager
+ *
+ * Generates random images.
+ *
+ */
+class dummy_block_manager : public async_manager<encoded_record_list, encoded_record_list>
+{
+    class generator : virtual async_manager_source<encoded_record_list>
+    {
+        generator() {}
+        virtual ~generator() {}
+        virtual encoded_record_list* next()                      = 0;
+        virtual size_t               record_count() const        = 0;
+        virtual size_t               elements_per_record() const = 0;
+        virtual void                 reset()                     = 0;
+        virtual void                 suspend_output() {}
+    };
+
+public:
+    dummy_block_manager(size_t block_size,
+                        size_t block_count,
+                        size_t record_count,
+                        size_t elements_per_record)
+        : async_manager<encoded_record_list, encoded_record_list>{make_shared<async_manager_source<
+                                                                      encoded_record_list>>(),
+                                                                  "dummy_block_manager"}
+        , m_current_block_number{0}
+        , m_block_size{block_size}
+        , m_block_count{block_count}
+        , m_record_count{record_count}
+        , m_elements_per_record{elements_per_record}
+        , m_random_generator{random_device{}()}
+    {
+    }
+
+    virtual ~dummy_block_manager() { finalize(); }
+    encoded_record_list* filler() override
+    {
+        m_state                    = async_state::wait_for_buffer;
+        encoded_record_list* rc    = get_pending_buffer();
+        m_state                    = async_state::processing;
+        encoded_record_list* input = nullptr;
+
+        rc->clear();
+
+        m_state = async_state::fetching_data;
+        input   = m_source->next();
+        m_state = async_state::processing;
+        if (input == nullptr)
+        {
+            rc = nullptr;
+        }
+        else
+        {
+            input->swap(*rc);
+        }
+
+        if (++m_current_block_number == m_block_count)
+        {
+            m_current_block_number = 0;
+            m_source->reset();
+        }
+
+        if (rc && rc->size() == 0)
+        {
+            rc = nullptr;
+        }
+
+        m_state = async_state::idle;
+        return rc;
+    }
+
+    virtual void initialize() override { m_current_block_number = 0; }
+    size_t       record_count() const override { return m_block_size; }
+    size_t       elements_per_record() const override { return m_elements_per_record; }
+private:
+    size_t            m_current_block_number;
+    size_t            m_block_size;
+    size_t            m_block_count;
+    size_t            m_record_count;
+    size_t            m_elements_per_record;
+    std::minstd_rand0 m_random_generator;
+    bool              m_enable_shuffle;
+};
+
+TEST(benchmark, decode_and_transform)
+{
+    char* bsz          = getenv("TEST_IMAGENET_BATCH_SIZE");
+    char* thread_count = getenv("TEST_IMAGENET_DECODE_THREAD_COUNT");
+
+    size_t batch_size          = 128;
+    size_t decode_thread_count = 0;
+    size_t block_size          = 5005;
+    size_t record_count        = 1281167;
+    size_t block_count         = 256;
+    size_t elements_per_record;
+    if (bsz)
+    {
+        std::istringstream iss(bsz);
+        iss >> batch_size;
+    }
+    if (thread_count)
+    {
+        std::istringstream iss(thread_count);
+        iss >> decode_thread_count;
+    }
+
+    auto manager = make_shared<dummy_block_manager>(
+        block_size, block_count, record_count, elements_per_record);
+
+    // Default ceil div to get number of batches
+    // m_batch_count_value = (record_count + batch_size - 1) / batch_size;
+    // m_batch_mode        = BatchMode::ONCE;
+    using nlohmann::json;
+
+    int height;
+    int width;
+
+    json image_config = {{"type", "image"},
+                         {"height", height},
+                         {"width", width},
+                         {"channels", 3},
+                         {"output_type", "float"},
+                         {"channel_major", true},
+                         {"bgr_to_rgb", true}};
+
+    json label_config = {{"type", "label"}, {"binary", false}};
+
+    auto aug_config = vector<json>{{{"type", "image"},
+                                    {"flip_enable", true},
+                                    {"center", false},
+                                    {"crop_enable", true},
+                                    {"horizontal_distortion", {3. / 4., 4. / 3.}},
+                                    {"do_area_scale", true},
+                                    {"scale", {0.08, 1.0}},
+                                    {"mean", {0.485, 0.456, 0.406}},
+                                    {"stddev", {0.229, 0.224, 0.225}},
+                                    {"resize_short_size", 0}}};
+
+    json config = {{"manifest_root", ""},
+                   {"manifest_filename", ""},
+                   {"shuffle_enable", true},
+                   {"shuffle_manifest", true},
+                   {"batch_size", batch_size},
+                   {"iteration_mode", "INFINITE"},
+                   {"cache_directory", ""},
+                   {"decode_thread_count", 0},
+                   {"etl", {image_config, label_config}},
+                   {"augmentation", aug_config}};
+
+    auto m_provider = provider_factory::create(config_json);
+
+    unsigned int threads_num =
+        decode_thread_count != 0 ? decode_thread_count : std::thread::hardware_concurrency();
+
+    const int decode_size = batch_size * ((threads_num * m_input_multiplier - 1) / batch_size + 1);
+    m_batch_iterator      = make_shared<batch_iterator>(manager, decode_size);
+
+    m_decoder = make_shared<batch_decoder>(m_batch_iterator,
+                                           decode_size,
+                                           decode_thread_count,
+                                           false /*pinned*/,
+                                           m_provider,
+                                           random_seed);
+
+    m_final_stage =
+        make_shared<batch_iterator_fbm>(m_decoder, batch_size, m_provider, !batch_major);
+
+    m_output_buffer_ptr = m_final_stage->next();
 }
 
 // TODO(sfraczek): move benchmarks from test_loader.cpp and other test files here
